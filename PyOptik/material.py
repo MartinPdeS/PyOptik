e1408082bf9e079aa42d0714af3170504cd3e59b
MartinPdeS <Martin.Poinsinet.de.Sivry@gmail.com>
2024-09-01 13:00:12 +0200

    adding usual materials singleton

--- PyOptik/material.py
+++ PyOptik/material.py
@@ -19,50 +19,62 @@ class UsualMaterial:
         'germanium',
     ]
 
+    @staticmethod
     @property
     def silver(self):
         return TabulatedMaterial('silver')
 
+    @staticmethod
     @property
     def gold(self):
         return TabulatedMaterial('gold')
 
+    @staticmethod
     @property
     def aluminium(self):
         return TabulatedMaterial('aluminium')
 
+    @staticmethod
     @property
     def copper(self):
         return TabulatedMaterial('copper')
 
+    @staticmethod
     @property
     def zinc(self):
         return TabulatedMaterial('zinc')
 
+    @staticmethod
     @property
     def iron(self):
         return TabulatedMaterial('iron')
 
+    @staticmethod
     @property
     def argon(self):
         return SellmeierMaterial('argon')
 
+    @staticmethod
     @property
     def water(self):
         return SellmeierMaterial('water')
 
+    @staticmethod
     @property
     def silicon(self):
         return SellmeierMaterial('silicon')
 
+    @staticmethod
     @property
     def BK7(self):
         return SellmeierMaterial('BK7')
 
+    @staticmethod
     @property
     def fused_silica(self):
         return SellmeierMaterial('fused_silica')
 
+    @staticmethod
     @property
     def germanium(self):
         return SellmeierMaterial('germanium')
--- tests/test_usual_material.py
+++ tests/test_usual_material.py
@@ -7,6 +7,7 @@ def test_usual_material(material_name):
     Test each usual material defined in UsualMaterial to ensure that it can be instantiated without errors.
     """
     material_instance = getattr(UsualMaterial, material_name)
+
     assert material_instance is not None, f"{material_name} instantiation failed."
 
 if __name__ == "__main__":

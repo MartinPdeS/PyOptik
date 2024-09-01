61493eba6d8a042b5aec020996242201db72dd9f
MartinPdeS <Martin.Poinsinet.de.Sivry@gmail.com>
2024-09-01 12:53:07 +0200

    adding usual materials singleton

---
+++ tests/test_usual_material.py
@@ -0,0 +1,13 @@
+import pytest
+from PyOptik import UsualMaterial
+
+@pytest.mark.parametrize('material_name', UsualMaterial.all, ids=lambda name: f'Test {name}')
+def test_usual_material(material_name):
+    """
+    Test each usual material defined in UsualMaterial to ensure that it can be instantiated without errors.
+    """
+    material_instance = getattr(UsualMaterial, material_name)
+    assert material_instance is not None, f"{material_name} instantiation failed."
+
+if __name__ == "__main__":
+    pytest.main([__file__])

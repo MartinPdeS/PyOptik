c681d6f307bdbbd87241e84bb4767c3dde8f2277
MartinPdeS <Martin.Poinsinet.de.Sivry@gmail.com>
2024-09-03 09:06:08 +0200

    small adjustments plus adding __main__.py

--- tests/test_usual_material.py
+++ tests/test_usual_material.py
@@ -2,7 +2,7 @@ import pytest
 from PyOptik import UsualMaterial
 from PyOptik.material import SellmeierMaterial, TabulatedMaterial
 
-@pytest.mark.parametrize('material_name', UsualMaterial.all, ids=lambda name: f'Test {name}')
+@pytest.mark.parametrize('material_name', UsualMaterial.all_materials, ids=lambda name: f'Test {name}')
 def test_usual_material(material_name):
     """
     Test each usual material defined in UsualMaterial to ensure that it can be instantiated without errors.
@@ -11,7 +11,7 @@ def test_usual_material(material_name):
 
     assert isinstance(material_instance, (SellmeierMaterial, TabulatedMaterial)), f"{material_name} instantiation failed."
 
-@pytest.mark.parametrize('material_name', UsualMaterial.all, ids=lambda name: f'Test {name}')
+@pytest.mark.parametrize('material_name', UsualMaterial.all_materials, ids=lambda name: f'Test {name}')
 def test_get_material(material_name):
     """
     Test each usual material defined in UsualMaterial to ensure that it can be instantiated without errors.

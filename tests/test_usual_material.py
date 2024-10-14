2653736f1eac018aa0c2f3998b4cb036981d0fd4
MartinPdeS <Martin.Poinsinet.de.Sivry@gmail.com>
2024-10-14 18:10:25 +0200

    fix on the unittests

--- tests/test_usual_material.py
+++ tests/test_usual_material.py
@@ -2,7 +2,7 @@ import pytest
 from PyOptik import Material
 from PyOptik.material import SellmeierMaterial, TabulatedMaterial
 
-@pytest.mark.parametrize('material_name', Material.all, ids=lambda name: f'Test {name}')
+@pytest.mark.parametrize('material_name', Material.all, ids=lambda name: f'{name}')
 def test_usual_material(material_name):
     """
     Test each usual material defined in UsualMaterial to ensure that it can be instantiated without errors.

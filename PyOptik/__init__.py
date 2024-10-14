698dc2ddd205b2838dceb6929986294620131ebb
MartinPdeS <Martin.Poinsinet.de.Sivry@gmail.com>
2024-10-14 18:13:43 +0200

    adding oneline for retro-compatibility

--- PyOptik/__init__.py
+++ PyOptik/__init__.py
@@ -9,5 +9,6 @@ from .utils import build_default_library
 from .material import Material
 from .material import TabulatedMaterial
 from .material import SellmeierMaterial
+from .material import base_class
 
 Material = Material()

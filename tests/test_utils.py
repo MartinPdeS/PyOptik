dddcf6c1e6fee1cc6afef35ef8f1a4cb6c47be91
MartinPdeS <martin.poinsinet.de.sivry@gmail.com>
2025-07-12 04:44:36 +0200

    small change to unittest

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -14,48 +14,48 @@ def test_dummy():
     """Dummy test to ensure pytest runs correctly."""
     assert True
 
-# def test_main():
-#     MaterialBank.build_library('minimal', remove_previous=True)
+def test_main():
+    MaterialBank.build_library('minimal', remove_previous=True)
 
-# url_water = 'https://refractiveindex.info/database/data/main/H2O/nk/Daimon-19.0C.yml'
+url_water = 'https://refractiveindex.info/database/data/main/H2O/nk/Daimon-19.0C.yml'
 
-# def test_download_yml_files():
-#     """
-#     Test downloading YAML files to different locations. Ensures that files are
-#     correctly downloaded from a given URL to the specified directory.
-#     """
-#     download_yml_file(
-#         filename='test_tabulated',
-#         url=url_water,
-#         location=MaterialType.TABULATED
-#     )
+def test_download_yml_files():
+    """
+    Test downloading YAML files to different locations. Ensures that files are
+    correctly downloaded from a given URL to the specified directory.
+    """
+    download_yml_file(
+        filename='test_tabulated',
+        url=url_water,
+        location=MaterialType.TABULATED
+    )
 
-#     download_yml_file(
-#         filename='test_sellmeier',
-#         url=url_water,
-#         location=MaterialType.SELLMEIER
-#     )
-
-
-# def test_add_custom():
-#     MaterialBank.add_tabulated_to_bank(
-#         filename='test_tabulated',
-#         url=url_water,
-#     )
-
-#     MaterialBank.add_sellmeier_to_bank(
-#         filename='test_sellmeier',
-#         url=url_water,
-#     )
+    download_yml_file(
+        filename='test_sellmeier',
+        url=url_water,
+        location=MaterialType.SELLMEIER
+    )
 
 
-# def test_fail_add_custom():
-#     with pytest.raises(ValueError):
-#         MaterialBank.add_material_to_bank(
-#             filename='test_tabulated',
-#             url=url_water,
-#             material_type='invalid_location'
-#         )
+def test_add_custom():
+    MaterialBank.add_tabulated_to_bank(
+        filename='test_tabulated',
+        url=url_water,
+    )
+
+    MaterialBank.add_sellmeier_to_bank(
+        filename='test_sellmeier',
+        url=url_water,
+    )
+
+
+def test_fail_add_custom():
+    with pytest.raises(ValueError):
+        MaterialBank.add_material_to_bank(
+            filename='test_tabulated',
+            url=url_water,
+            material_type='invalid_location'
+        )
 
 
 
